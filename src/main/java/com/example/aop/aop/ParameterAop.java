d07b13aed0396563d26c006283aec63b58e17759
yunseung <yseungun.21@gmail.com>
2024-02-13 18:49:42 +0900

    feat: log용 aop 적용

--- src/main/java/com/example/aop/aop/ParameterAop.java
+++ src/main/java/com/example/aop/aop/ParameterAop.java
@@ -1,2 +1,38 @@
-package com.example.aop.aop;public class ParameterAop {
+package com.example.aop.aop;
+
+import java.lang.reflect.Method;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Aspect // AOP로 동작시킴
+@Component // 스프링에서 관리
+public class ParameterAop {
+
+    @Pointcut("execution(* com.example.aop.controller..*.*(..))")
+    private void cut() {}
+
+    @Before("cut()")
+    public void before(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        System.out.println(method.getName());
+
+        Object[] args = joinPoint.getArgs();
+
+        for(Object obj : args) {
+            System.out.println("type" + obj.getClass().getSimpleName());
+            System.out.println("value" + obj);
+        }
+    }
+
+    @AfterReturning(value = "cut()", returning = "returnObj")
+    public void afterReturn(JoinPoint joinPoint, Object returnObj) {
+        System.out.println("return obj");
+        System.out.println(returnObj);
+    }
 }
