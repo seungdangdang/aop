861eb40ff68a88740a2bdf16271bfdb4360acf59
yunseung <yseungun.21@gmail.com>
2024-02-13 19:26:45 +0900

    feat: 타임스탬프 aop 작성

--- src/main/java/com/example/aop/controller/RestApiController.java
+++ src/main/java/com/example/aop/controller/RestApiController.java
@@ -1,6 +1,8 @@
 package com.example.aop.controller;
 
+import com.example.aop.annotation.Timer;
 import com.example.aop.dto.User;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,5 +24,12 @@ public class RestApiController {
     public User post(@RequestBody User user) {
         System.out.println("post method" + user);
         return user;
+    }
+
+    @Timer
+    @DeleteMapping("/delete")
+    public void delete() throws InterruptedException {
+
+        Thread.sleep(1000 * 2);
     }
 }
